7a5d90e5dbd7ac9dc8f4b706f9f842868022f3b1
farhanyp <farhan.yudha2016@gmail.com>
2026-04-18 23:57:14 +0700

    refactor: remove Siddiq & Tiara invitation pages and template data

--- app/invitation/siddiq-tiara/beranda/rsvp/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ProfessionalOneRsvpPage } from "@/app/_components/template-wedding-invitation/professional-1";
-
-export default function SiddiqTiaraBerandaRsvpPage() {
-  return <ProfessionalOneRsvpPage />;
-}
--- app/invitation/siddiq-tiara/page.tsx
+++
@@ -1,6 +0,0 @@
-import { InvitationGate } from "@/app/_components/template-wedding-invitation/professional-1";
-import { siddiqTiaraTemplateData } from "./template-data";
-
-export default function SiddiqTiaraPage() {
-  return <InvitationGate data={siddiqTiaraTemplateData} />;
-}
