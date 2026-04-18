7eae911c02909ee4fc69e7dadb34162654931e9a
farhanyp <farhan.yudha2016@gmail.com>
2026-04-19 03:26:28 +0700

    add edge runtime

--- app/preview/professional-1/naruto-hinata/beranda/page.tsx
+++ app/preview/professional-1/naruto-hinata/beranda/page.tsx
@@ -1,4 +1,5 @@
 ﻿export const dynamic = "force-dynamic";
+export const runtime = "edge";
 
 import { ProfessionalOneLandingPage } from "@/app/_components/template-wedding-invitation/professional-1";
 import { getProfessionalOnePreviewData } from "../data";
